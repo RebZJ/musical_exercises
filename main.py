b204b25f30adde8e7614a940ee3e047b3fb3bd90
RebZJ <rebontoz@gmail.com>
2019-12-28 23:45:03 +1100

    loop

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from random_interval import *
 
 def execute_exercises():
-    print("how many of each exercise do you want to generate?")
-    input1 = int(input()) 
-    
-    chord_notes(input1)
+    do_again = True
 
-    do_intervals(input1,"sing")
+    while do_again == True:
+        print("how many of each exercise do you want to generate?")
+        input1 = int(input()) 
+        
+        chord_notes(input1)
+
+        do_intervals(input1,"sing")
+
+        print("Generate another batch? 'y' or 'n' ")
+        repeat = str(input()) 
+
+        if repeat == 'y':
+            do_again = True
+        elif repeat == 'n':
+            do_again = False
 
 if __name__ == "__main__":
     execute_exercises()
